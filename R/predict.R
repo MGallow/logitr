9ed35cbb9674be9441c47311a2c8fb77c20b5c88
MGallow <gall0441@umn.edu>
2017-05-18 18:12:21 -0500

    fixed log loss

--- R/predict.R
+++ R/predict.R
@@ -17,49 +17,49 @@
 
 
 predict_logisticr = function(object, X, y = NULL) {
-    
+
     # checks
     X = as.matrix(X)
     if (!is.null(y)) {
         y = as.matrix(y)
-        if (nrow(X) != nrow(y)) 
+        if (nrow(X) != nrow(y))
             stop("X and y must have equal observations!")
-        
-        if (all(y == 1 | y == 0) == FALSE) 
+
+        if (all(y == 1 | y == 0) == FALSE)
             stop("y must be binary!")
     }
-    
+
     # if object is list, extract betas
     if (class(object) == "list") {
         object = object$coefficients
     }
-    
+
     # add intercept, if needed
     if (ncol(X) != nrow(object)) {
         X = cbind(1, X)
     }
-    
+
     # fitted values
     fitted = logitr(X %*% object)
     class = round(fitted)
-    
+
     # if y, return MSE, misclassification
     MSE = NULL
-    Log.loss = NULL
+    log.loss = NULL
     misclassification = NULL
     if (!is.null(y)) {
-        
+
         # calculate metrics
         MSE = mean((y - fitted)^2)
-        Log.loss = sum(-y * log(fitted) - (1 - y) * log(1 - 
+        log.loss = sum(-y * log(fitted) - (1 - y) * log(1 -
             fitted))
         misclassification = mean(y != class)
-        
+
     }
-    
-    
-    returns = list(fitted.values = fitted, class = class, 
-        MSE = MSE, Log.loss = Log.loss, misclassification = misclassification)
+
+
+    returns = list(fitted.values = fitted, class = class,
+        MSE = MSE, log.loss = log.loss, misclassification = misclassification)
     return(returns)
 }
 
@@ -85,40 +85,40 @@ predict_logisticr = function(object, X, y = NULL) {
 
 
 predict_linearr = function(object, X, y = NULL) {
-    
+
     # checks
     X = as.matrix(X)
     if (!is.null(y)) {
         y = as.matrix(y)
-        if (nrow(X) != nrow(y)) 
+        if (nrow(X) != nrow(y))
             stop("X and y must have equal observations!")
     }
-    
+
     # if object is list, extract betas
     if (class(object) == "list") {
         object = object$coefficients
     }
-    
+
     # add intercept, if needed
     if (ncol(X) != nrow(object)) {
         X = cbind(1, X)
     }
-    
+
     # fitted values
     fitted = X %*% object
-    
+
     # if y, return MSE, misclassification
     RSS = NULL
     MSE = NULL
     if (!is.null(y)) {
-        
+
         # calculate metrics
         RSS = sum((y - fitted)^2)
         MSE = mean((y - fitted)^2)
-        
+
     }
-    
-    
+
+
     returns = list(fitted.values = fitted, RSS = RSS, MSE = MSE)
     return(returns)
 }
